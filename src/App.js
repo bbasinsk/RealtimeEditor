1ed4f1f2a72778bd3772e7f691bc2dda5176a41d
benbasinski97@gmail.com <benbasinski97@gmail.com>
2017-12-18 15:33:01 -0800

    Added codemirror package

---
+++ src/App.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+import './App.css';
+import CodeMirror from 'react-codemirror';
+import 'codemirror/lib/codemirror.css';
+
+
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { code: "// Code" };
+  }
+
+  getInitialState() {
+		return {
+			code: "// Code",
+		};
+	}
+
+	updateCode = (newCode) => {
+		this.setState({ code: newCode });
+	}
+
+
+  render() {
+    let options = {
+			lineNumbers: true,
+    };
+
+    return (
+
+      <div className="App">
+        <CodeMirror
+          value={this.state.code} 
+          onChange={this.updateCode} 
+          options={options} />
+      </div>
+    );
+  }
+}
+
+export default App;
